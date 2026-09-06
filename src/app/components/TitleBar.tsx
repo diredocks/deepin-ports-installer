52c3a4bcbe6d5f618b95e21521616a311f5ab146
diredocks <26994007+diredocks@users.noreply.github.com>
2026-09-06 19:10:08 +0800

    feat: add title and logo to title bar

--- src/app/components/TitleBar.tsx
+++ src/app/components/TitleBar.tsx
@@ -1,10 +1,17 @@
 import { mdiClose, mdiWindowMinimize } from "@mdi/js";
 import { Icon } from "~/components/Icon";
+import logo from "~/components/assets/logo.svg";
 
 export function TitleBar() {
   return (
     <div class="h-8 flex items-center select-none">
-      <div class="flex-1 h-full" style={{ "-webkit-app-region": "drag" }} />
+      <div
+        class="flex-1 h-full flex items-center pl-3 text-sm font-medium text-gray-600"
+        style={{ "-webkit-app-region": "drag" }}
+      >
+        <img src={logo} alt="Deepin logo" class="h-5 w-5" />
+        <span class="ml-2">Deepin Ports Installer</span>
+      </div>
       <div class="flex h-full" style={{ "-webkit-app-region": "no-drag" }}>
         <button
           class="w-12 h-full flex items-center justify-center
