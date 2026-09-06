c6e2df59a35cee5d65279150dacf8ca570055109
diredocks <26994007+diredocks@users.noreply.github.com>
2026-09-06 14:59:37 +0800

    refactor: move renderer and preload sources into src/app

--- forge.config.ts
+++ forge.config.ts
@@ -30,7 +30,7 @@ const config: ForgeConfig = {
           target: 'main',
         },
         {
-          entry: 'src/preload.ts',
+          entry: 'src/app/preload.ts',
           config: 'vite.preload.config.ts',
           target: 'preload',
         },
--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -1,4 +1,7 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
+import path from "node:path";
 
 // https://vitejs.dev/config
-export default defineConfig({});
+export default defineConfig({
+  root: path.resolve(__dirname, "src/app"),
+});
