a2266b4c0ad46c98b6584c942358518e953bc3b8
diredocks <26994007+diredocks@users.noreply.github.com>
2026-09-06 15:05:15 +0800

    refactor: use ~ alias for renderer imports

--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from "vite";
 import path from "node:path";
+import { fileURLToPath } from "node:url";
 
 // https://vitejs.dev/config
 export default defineConfig({
   root: path.resolve(__dirname, "src/app"),
+  resolve: {
+    alias: {
+      "~": fileURLToPath(new URL("./src/app", import.meta.url)),
+    },
+  },
 });
